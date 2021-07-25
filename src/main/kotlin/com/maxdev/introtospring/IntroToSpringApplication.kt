ef7e5dc08e1f0de581795255e58841ff3ba524f0
MaksatRysbekov <maksatrysbekov96@gmail.com>
2021-07-25 22:40:47 +0200

    first commit

---
+++ src/main/kotlin/com/maxdev/introtospring/IntroToSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.maxdev.introtospring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntroToSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<IntroToSpringApplication>(*args)
+}
